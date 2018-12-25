40fcc0708dd805370ce87719f0d5ae665317b049
Steve Wagner <ciroque@live.com>
2018-12-25 09:29:28 -0800

    List now renders items

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './list.css';
 
-const ListItem = (props) => (<div className="sk-li">{props.text}</div>);
+const ListItem = (props) => (<div key={props.key} className="sk-list-item">{props.text}</div>);
 
 export default class List extends React.Component {
     constructor(props) {
         super(props);
-
+        this.state = {
+            items: props.items || []
+        }
     }
 
     render = () => {
-        return (<div className='sk-list'/>)
+        return (
+            <div className='sk-list'>
+                {this.state.items.map(item => ListItem({key: item, text: item}))}
+            </div>
+        );
     }
 };
--- src/components/list/list.spec.js
+++ src/components/list/list.spec.js
@@ -9,3 +9,15 @@ it('renders an empty list', () => {
     expect(list.children.length).toEqual(0);
     ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders a single item', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<List items={['One']}/>, div);
+    expect(div.children[0].children.length).toEqual(1);
+});
+
+it('renders three items', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<List items={['One', 'Two', 'Three']}/>, div);
+    expect(div.children[0].children.length).toEqual(3);
+});
