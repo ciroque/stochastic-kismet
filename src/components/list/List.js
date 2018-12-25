0402e0f8bb2088460d4ec5704c9c68cf3e390c0f
Steve Wagner <ciroque@live.com>
2018-12-25 15:55:57 -0800

    Now selects and sorts and selects

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import './List.css';
 
-const ListItem = (props) => (<div key={props.key} className="sk-list-item">{props.text}</div>);
+class ListItem extends React.Component {
+    render = () => {
+        return (
+            <div
+                onClick={() => this.props.itemClicked(this.props)}
+                className={this.props.selected ? 'sk-selected-list-item' : 'sk-list-item'}
+            >{this.props.text}</div>
+        );
+    }
+}
 
 export default class List extends React.Component {
+    handleItemClicked = (item) => this.props.itemClicked ? this.props.itemClicked(item) : console.warn('no item clicked handler defined...') ;
     render = () => {
         return (
             <div className='sk-list'>
-                {this.props.items.map(item => ListItem({key: item, text: item}))}
+                {this.props.items.map(item => <ListItem itemClicked={this.handleItemClicked} key={item.tag} {...item} />)}
             </div>
         );
     }
--- src/components/listManager/ListManager.js
+++ src/components/listManager/ListManager.js
@@ -10,26 +10,31 @@ export default class ListManager extends React.Component {
 
         this.state = {
             items: [
-                '.',
-                '..',
-                '...',
-                '....',
-                '.....'
+                {tag: 0, text: '.', selected: false },
+                {tag: 1, text: '..', selected: false },
+                {tag: 2, text: '...', selected: false },
+                {tag: 3, text: '....', selected: false },
+                {tag: 4, text: '.....', selected: false },
+                {tag: 5, text: '......', selected: false },
+                {tag: 6, text: '.......', selected: false },
+                {tag: 7, text: '........', selected: false },
+                {tag: 8, text: '.........', selected: false },
+                {tag: 9, text: '..........', selected: false },
             ],
             shuffledItems: []
         }
     }
 
     addItem = () => {
-        const newItem = new Date().getMilliseconds();
-        console.log(`...>>> ${newItem}`);
-        this.setState((ps) => ({items: [...ps.items, newItem]}), () => console.warn(this.state.items));
+        // const newItem = new Date().getMilliseconds();
+        // console.log(`...>>> ${newItem}`);
+        // this.setState((ps) => ({items: [...ps.items, newItem]}), () => console.warn(this.state.items));
     };
 
     // Fisher-Yates shuffle: http://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
     // see also: http://www.i-programmer.info/programming/theory/2744-how-not-to-shuffle-the-kunth-fisher-yates-algorithm.html
     fisherYates = (list) => {
-        const shuffled = list.slice(0);
+        const shuffled = Array.from(list, i => Object.assign({}, i)); // list.slice(0);
         const length = shuffled.length - 1;
         for (var index = length; index > 0; index--) {
             const randomIndex = Math.floor(Math.random() * (index + 1));
@@ -40,27 +45,82 @@ export default class ListManager extends React.Component {
         return shuffled;
     };
 
-    reset = () => this.setState({shuffledItems: []});
+    reset = () => {
+        this.setState({shuffledItems: []});
+        this.state.items.forEach(i => i.selected = false);
+    };
+
+    selectOne = () => {
+        const length = this.state.shuffledItems.length;
+        if(length > 0) {
+            const updatedItems = Array.from(this.state.shuffledItems);
+            updatedItems.forEach(i => i.selected = false);
+            var selectedIndex = Math.floor((Math.random() * length));
+            this.toggleSelectedAt(updatedItems, selectedIndex);
+            this.setState({shuffledItems: updatedItems});
+        }
+    };
 
     shuffle = () => {
         if(this.state.items.length > 0) {
             const shuffled = this.fisherYates(this.state.items);
+            shuffled.forEach(i => i.selected = false);
             this.setState({shuffledItems: shuffled});
         }
+    };
+
+    shuffleAndSelect = () => {
+        new Promise((resolve) => {
+            this.shuffle();
+            resolve();
+        }).then(() => this.selectOne())
+    };
+
+    sourceItemClicked = (item) => {
+        const updatedItems = Array.from(this.state.items); // this.state.items.splice(0);
+        const selectedIndex = updatedItems.findIndex(i => i.tag === item.tag);
+        this.toggleSelectedAt(updatedItems, selectedIndex);
+        this.setState({items: updatedItems});
+    };
+
+    toggleSelectedAt = (items, index) => {
+        const updatedItem = items[index];
+        updatedItem.selected = !updatedItem.selected;
+        items.splice(index, 1, updatedItem);
+        return items;
     };
 
     render = () => {
         return (
             <div className='list-manager'>
-                <div className='source-list-label'><Label text='Source List' /></div>
-                <div className='source-list'><List items={this.state.items}/></div>
+                <div className='source-list-label'>
+                    <Label text='Source List' />
+                </div>
+                <div className='source-list'>
+                    <List
+                        itemClicked={this.sourceItemClicked}
+                        items={this.state.items}/>
+                </div>
 
-                <div className='shuffle-button'><ActionButton text='Shuffle' clickHandler={() => this.shuffle()}/></div>
-                <div className='select-button'><ActionButton text='Select' clickHandler={() => this.addItem()}/></div>
-                <div className='reset-button'><ActionButton text='Reset' clickHandler={() => this.reset()}/></div>
+                <div className='shuffle-button'>
+                    <ActionButton text='Shuffle' clickHandler={() => this.shuffle()}/>
+                </div>
+                <div className='select-button'>
+                    <ActionButton text='Select' clickHandler={() => this.selectOne()}/>
+                </div>
+                <div className='shuffle-select-button'>
+                    <ActionButton text='Shuffle / Select' clickHandler={() => this.shuffleAndSelect()}/>
+                </div>
+                <div className='reset-button'>
+                    <ActionButton text='Reset' clickHandler={() => this.reset()}/>
+                </div>
 
-                <div className='shuffled-list-label'><Label text='Shuffled List' /></div>
-                <div className='shuffled-list'><List items={this.state.shuffledItems}/></div>
+                <div className='shuffled-list-label'>
+                    <Label text='Shuffled List' />
+                </div>
+                <div className='shuffled-list'>
+                    <List items={this.state.shuffledItems}/>
+                </div>
             </div>
         );
     }
